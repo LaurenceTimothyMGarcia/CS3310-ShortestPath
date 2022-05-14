f70ceba35f5b4c32346c97bae730ff5ad02f125c
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-05-14 07:41:31 -0700

    input keyboard input

--- ShortestPath.java
+++ ShortestPath.java
@@ -1,2 +1,31 @@
 //Laurence Timothy M. Garcia
-//
+//CS 3310.01
+//Professor Huong Luu
+//Project 2 - Shortest Path
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class ShortestPath
+{
+    public static void main(String[] args)
+    {
+        //Keyboard input
+        Scanner kb = new Scanner(System.in);
+
+        int graphSize = keyboardInput(kb);
+
+        int graph[][] = new int[][];
+    }
+
+    public static int keyboardInput(Scanner kb)
+    {
+        int input = 0;
+
+        System.out.println("Please input quantity of vertices in graph");
+        input = kb.nextInt();
+
+        return input;
+    }
+}
